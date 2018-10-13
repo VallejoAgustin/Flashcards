8c92a0d2d63bac0b5954caab715bbae2ed0531cd
Agustin Vallejo <agustinvallejo@agustins-macbook-pro.local>
2018-10-13 15:32:16 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Agustin Vallejo on 10/13/18.
+//  Copyright © 2018 Agustin Vallejo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
